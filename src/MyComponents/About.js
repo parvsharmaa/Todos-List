dfa2cb33f9834fb0a6f33c604ae7250c7139f3e2
Parv <parv31oct@gmail.com>
2021-06-24 20:14:13 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis provident, quos expedita quam suscipit, assumenda minima excepturi voluptate fuga nesciunt illo quasi, blanditiis repellendus ea iste sunt architecto quo veniam.</p>
+        </div>
+    )
+}
